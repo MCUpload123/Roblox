f447d900d7e4312448e14ed62f5e62bb98f1585a
MCUpload123 <eqqqq730@gmail.com>
2026-03-04 19:16:36 +0700

    Update survive tsunami for brainrots.lua

--- survive tsunami for brainrots.lua
+++ survive tsunami for brainrots.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet("https://my-site-c94ot68a-zenkun664.wix-vibe-site.com/raw/k7r3sj85e"))()
 
+-- api
 loadstring(game:HttpGet("https://my-site-c94ot68a-zenkun664.wix-vibe-site.com/raw/sjnu3ruou"))()
